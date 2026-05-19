832b44fbd6c19ac546559435ed97221c053903d0
rashii15 <rashindilavanya@gmail.com>
2026-05-19 13:34:21 +0530

    added hight priority system

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -10,4 +10,12 @@ class TaskModel {
     required this.deadline,
     this.isCompleted = false,
   });
+  String get priority {
+    final difference = deadline.difference(DateTime.now()).inDays;
+
+    if (isCompleted) return "Completed";
+    if (difference < 2) return "High";
+    if (difference < 5) return "Medium";
+    return "Low";
+  }
 }
